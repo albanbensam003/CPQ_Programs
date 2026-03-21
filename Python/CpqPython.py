75273fe8a90df1e75024281853d8452e1c20592f
albanbensam003 <albenbensamb.tech@gmail.com>
2026-03-21 14:25:12 +0530

    First commit

---
+++ Python/CpqPython.py
@@ -0,0 +1,38 @@
+# Insert Custom table records
+set_data = [
+    {
+        "emp_name": "Test Data",
+        "emp_id": 1001
+    },{
+        "emp_name": "Test Data 1",
+        "emp_id": 1002
+    }
+]
+
+get_ct_tbl = SqlHelper.GetTable("CT_Employee")
+
+for data in set_data:
+    get_ct_tbl.AddRow(data)
+
+SqlHelper.Upsert(get_ct_tbl)
+
+# Update custom table records
+employee_records = [
+    {
+        "emp_name": "Test data",
+        "CpqTableEntryId": 1
+    },{
+        "emp_name": "Test data 1",
+        "CpqTableEntryId": 2
+    },{
+        "emp_name": "Test data 2",
+        "CpqTableEntryId": 3
+    }
+]
+
+get_table_update = SqlHelper.GetTable('CT_Employee')
+
+for data in employee_records:
+    get_table_update.AddRow(data)
+
+SqlHelper.Upsert(get_table_update)
